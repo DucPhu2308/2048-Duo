5a8e5aa8eec5944bec2228b5eae62cdd1df870e1
Duong0907 <thanhduongphan12345@gmail.com>
2022-08-12 16:13:31 +0700

    fix bugs

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ canvas.height = 500
 canvas.style.border = "2px solid"
 
 const tileColor = {
-    '2': 'red',
+    '2': 'rgb(240, 213, 234)',
     '4': 'rgb(252, 104, 0)',
     '8': 'rgb(252, 150, 9)',
     '16': 'rgb(252, 239, 9)',
@@ -22,9 +22,9 @@ const tileColor = {
 
 function transpose(mat) {
     var new_mat = []
-    for (var i=0; i<4; i++) {
+    for (var i = 0; i < 4; i++) {
         new_mat.push([])
-        for (var j=0; j<4; j++) {
+        for (var j = 0; j < 4; j++) {
             new_mat[i].push(mat[j][i])
         }
     }
@@ -33,21 +33,23 @@ function transpose(mat) {
 
 function reverse(mat) {
     var new_mat = []
-    for (var i=0; i<4; i++) {
+    for (var i = 0; i < 4; i++) {
         new_mat.push([])
-        for (var j=0; j<4; j++) {
-            new_mat[i].push(mat[i][3-j])
+        for (var j = 0; j < 4; j++) {
+            new_mat[i].push(mat[i][3 - j])
         }
     }
     return new_mat
 }
 
-function showResult(state) {
-    if (state === 'WON' || 'LOST') {
-        resultBox.innerHTML = 'YOU ' + state + '!!!'
-        resultBox.didplay = 'block'
-    }
+function isFull(mat) {
+    for (let i = 0; i < 4; i++)
+        for (let j = 0; j < 4; j++)
+            if (mat[i][j] === 0)
+                return false
+    return true
 }
+
 class Player {
     constructor() {
         this.name
@@ -61,30 +63,29 @@ class Match {
         this.board
         this.winner
     }
-    
+
     initBoard() {
         this.board = []
-        for (var i=0; i<4; i++) {
-            this.board.push([0,0,0,0])
+        for (var i = 0; i < 4; i++) {
+            this.board.push([0, 0, 0, 0])
         }
-        var x = Math.floor(Math.random()*4)
-        var y = Math.floor(Math.random()*4)
+        var x = Math.floor(Math.random() * 4)
+        var y = Math.floor(Math.random() * 4)
         this.board[x][y] = 2
     }
     addNewTile() {
-        var x = Math.floor(Math.random()*4)
-        var y = Math.floor(Math.random()*4)
+        var x = Math.floor(Math.random() * 4)
+        var y = Math.floor(Math.random() * 4)
         while (this.board[x][y] !== 0) {
-            x = Math.floor(Math.random()*4)
-            y = Math.floor(Math.random()*4)
+            x = Math.floor(Math.random() * 4)
+            y = Math.floor(Math.random() * 4)
         }
         this.board[x][y] = 2
-        console.log('finding')
     }
     compressBoard() { // pull tiles to the left
         var new_board = []
-        for (var i=0; i<4; i++) {
-            new_board.push([0,0,0,0])
+        for (var i = 0; i < 4; i++) {
+            new_board.push([0, 0, 0, 0])
         }
         for (let i = 0; i < 4; i++) {
             var pos = 0
@@ -98,11 +99,11 @@ class Match {
         this.board = new_board
     }
     mergeTiles() { // merge after compress
-        for (var i=0; i<4; i++) {
-            for (var j=0; j<3; j++) {
-                if (this.board[i][j] == this.board[i][j+1]) {
+        for (var i = 0; i < 4; i++) {
+            for (var j = 0; j < 3; j++) {
+                if (this.board[i][j] == this.board[i][j + 1]) {
                     this.board[i][j] *= 2
-                    this.board[i][j+1] = 0
+                    this.board[i][j + 1] = 0
                     // score calculation here
                 }
             }
@@ -135,8 +136,8 @@ class Match {
         for (let i = 0; i < 4; i++) {
             for (let j = 0; j < 4; j++) {
                 if (this.board[i][j] !== 0) {
-                    let x = i * (tileSize + padding) + padding
-                    let y = j * (tileSize + padding) + padding
+                    let x = j * (tileSize + padding) + padding
+                    let y = i * (tileSize + padding) + padding
 
                     ctx.save()
                     ctx.strokeStyle = '#000'
@@ -147,7 +148,7 @@ class Match {
                     ctx.fillStyle = '#fff'
                     ctx.font = '50px Arial'
                     ctx.textAlign = 'center'
-                    ctx.fillText(`${this.board[i][j]}`, x + tileSize / 2, y + tileSize * (3/5));
+                    ctx.fillText(`${this.board[i][j]}`, x + tileSize / 2, y + tileSize * (3 / 5));
                     ctx.restore()
                 }
             }
@@ -155,28 +156,29 @@ class Match {
         console.log(this.getGameState())
     }
     addHandleInput() {
-        window.addEventListener('keydown',(e) => {
-                this.updateBoard(e.key)
-                var state = this.getGameState()
-                if (state == 'WON') {
-                    // win display
-                    showResult(state)
-                }
-                else if (state == 'GAME NOT OVER') {
-                    this.addNewTile()
-                }
-                else {
-                    // lose display
-                    showResult(state)
-                }
+        window.addEventListener('keydown', (e) => {
+            this.updateBoard(e.key)
+            var state = this.getGameState()
+            if (state == 'WON') {
+                // win display
+                this.showResult(state)
             }
+            else if (state == 'GAME NOT OVER') {
+                if (!isFull(this.board))
+                    this.addNewTile()
+            }
+            else {
+                // lose display
+                this.showResult(state)
+            }
+        }
         )
     }
     clearBoard() {
         ctx.clearRect(0, 0, canvas.width, canvas.height)
     }
     updateBoard(key) {
-        switch(key) {
+        switch (key) {
             case 'w':
                 this.moveUp()
                 break
@@ -192,31 +194,31 @@ class Match {
         }
     }
     getGameState() {
-        for (let i=0; i<4; i++) {
-            for (let j=0; j<4; j++) {
+        for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 4; j++) {
                 if (this.board[i][j] == 2048)
                     return 'WON'
             }
         }
-        for (let i=0; i<4; i++) {
-            for (let j=0; j<4; j++) {
+        for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 4; j++) {
                 if (this.board[i][j] == 0)
                     return 'GAME NOT OVER'
             }
         }
-        for (let i=0; i<3; i++) {
-            for (let j=0; j<3; j++) {
-                if (this.board[i][j] == this.board[i][j+1] || this.board[i][j] == this.board[i+1][j])
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (this.board[i][j] == this.board[i][j + 1] || this.board[i][j] == this.board[i + 1][j])
                     return 'GAME NOT OVER'
             }
         }
-        for (let i=0; i<3; i++) {
-            if (this.board[3][i] == this.board[3][i+1]) {
+        for (let i = 0; i < 3; i++) {
+            if (this.board[3][i] == this.board[3][i + 1]) {
                 return 'GAME NOT OVER'
             }
         }
-        for (let i=0; i<3; i++) {
-            if (this.board[i][3] == this.board[i+1][3]) {
+        for (let i = 0; i < 3; i++) {
+            if (this.board[i][3] == this.board[i + 1][3]) {
                 return 'GAME NOT OVER'
             }
         }
@@ -228,6 +230,11 @@ class Match {
             this.drawBoard()
             this.animate()
         })
+    }
+    showResult(state) {
+        this.clearBoard()
+        resultBox.innerHTML = 'YOU ' + state + '!!!'
+        resultBox.classList.remove('hidden')
     }
 }
 
